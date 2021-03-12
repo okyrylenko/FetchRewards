3453f5c7a030d4a401d4328b0e53e32aced754e8
oleksiykyrylenko <oleksiykyrylenkoautomation@gmail.com>
2021-03-12 15:22:52 -0500

    initial commit

---
+++ FetchRewards/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace FetchRewards    {
+    class Program        {
+        static void Main(string[] args)            {
+            Console.WriteLine("Hello World!");            }        }    }
