31bafde4f139c669bf73404d8ec46c3a102591d5
okyrylenko <40716729+okyrylenko@users.noreply.github.com>
2021-03-13 01:18:11 -0500

    Update IBasePage.cs

--- FetchRewards/IBasePage.cs
+++ FetchRewards/IBasePage.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;using OpenQA.Selenium;namespace FetchRewards
+using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+
+namespace FetchRewards
     {
     public interface IBasePage
         {
 
-        By LocateElementByCssSelector(string css);        By LocateElementById(string id);        IWebElement FindElement(By locator);        IWebElement FindNestedElement(IWebElement parent, By child);        IList<IWebElement> FindElements(By locator);
+        By LocateElementByCssSelector(string css);
+        By LocateElementById(string id);
+        IWebElement FindElement(By locator);
+        IWebElement FindNestedElement(IWebElement parent, By child);
+        IList<IWebElement> FindElements(By locator);
         IBasePage EnterText(By locator, string text);
         IBasePage EnterText(IWebElement element, string text);
         IBasePage ClickElement(By locator);
